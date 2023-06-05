f23e2abf60c6211b6c59c89bbc2acac43b1441d4
Ryan Salomão <ryansalomao@gmail.com>
2023-06-05 14:53:05 -0300

    add aula02.py file

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,37 @@
+""" Aula 02 - Keywords e identificadores """
+
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+# devem iniciar com uma letra ou _
+# não pode ter espaços em branco
+# não pode usar caracteres especiais !, @, #, $
+
+nome = "Maria"
+idade = "30"
+Nome = "João"
+nome_completo = "Maria da Silva"
+
+
+# Boas práticas (clean code)
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+PI = 3.14
+
+MAIORIDADE = 18
+idade = 15
+
+
+if idade >= MAIORIDADE:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
