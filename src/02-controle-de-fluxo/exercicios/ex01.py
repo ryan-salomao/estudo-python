6fc30a63b66d3b89baad156e06cf7082d068150f
Ryan Salomão <ryansalomao@gmail.com>
2023-06-12 15:20:37 -0300

    add exercicios folder & ex01.py file

---
+++ src/02-controle-de-fluxo/exercicios/ex01.py
@@ -0,0 +1,9 @@
+""" Exercício 01 - solicite ao usuário 3 notas e apresente o resultado da média aritmética das notas """
+
+n1 = float(input('Digite a nota 1: '))
+n2 = float(input('Digite a nota 2: '))
+n3 = float(input('Digite a nota 3: '))
+
+media = (n1 + n2 + n3) / 3
+
+print('Média: ', media)
