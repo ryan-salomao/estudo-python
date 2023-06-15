951b4a0521e9a8757f0562f8d6793832331ad484
Ryan Salomão <ryansalomao@gmail.com>
2023-06-15 04:09:34 -0300

    made debugger with breakpoint() & vscode debugger - ex05 Módulo 04

--- src/04-funcoes/exercicios/ex05.py
+++ src/04-funcoes/exercicios/ex05.py
@@ -1,51 +1,45 @@
 """ Exercício 05 """
 
 
+breakpoint()
 
-individuo = {
-    'altura': float(input("Digite seu peso em (Kg): ")),
-    'peso': float(input("Digite sua altura em (m): "))
+
+pessoa = {
+    "peso": float(input("Digite seu peso em (Kg): ")),
+    "altura": float(input("Digite sua altura em (m): "))
 }
 
 
 def calcular_imc(individuo):
-    """ retorna o imc de um individuo com base na altura e peso """
+    """retorna o imc de um individuo com base na altura e peso"""
 
     peso = individuo["peso"]
+
     altura = individuo["altura"]
 
+    imc = peso / (altura * altura)
 
-    IMC = peso / (altura * altura)
-    
-    return IMC
-
+    return imc
 
 
-def obter_classificacao(IMC):
-    """ retorna a classificação com base no imc """
+def obter_classificacao(imc):
+    """retorna a classificação com base no imc"""
 
     classificacao = ""
 
-
-    if IMC < 18.5:
-
+    if imc < 18.5:
         classificacao = "Baixo peso"
 
-
-    elif 18.5 <= IMC <= 24.9:
-
+    elif 18.5 <= imc <= 24.9:
         classificacao = "Peso normal"
 
-
-    elif 25.0 <= IMC <= 29.9:
+    elif 25.0 <= imc <= 29.9:
         classificacao = "Excesso de peso"
 
-
-    elif 30.0 <= IMC <= 34.9:
+    elif 30.0 <= imc <= 34.9:
         classificacao = "Obesidade de Classe 1"
 
-
-    elif 35.0 <= IMC <= 39.9:
+    elif 35.0 <= imc <= 39.9:
         classificacao = "Obesidade de Classe 2"
     else:
         classificacao = "Obesidade de Classe 3"
@@ -53,30 +47,24 @@ def obter_classificacao(IMC):
     return classificacao
 
 
-
-def situacao_individuo(IMC):
-    """ retorna a situação ('normal', 'perder peso', 'ganhar peso') com base no imc """
+def situacao_individuo(imc):
+    """retorna a situação ('normal', 'perder peso', 'ganhar peso') com base no imc"""
 
     situacao = ""
 
-
-    if IMC < 18.5:
+    if imc < 18.5:
         situacao = "ganhar peso"
 
-
-    elif 18.5 <= IMC <= 24.9:
+    elif 18.5 <= imc <= 24.9:
         situacao = "normal"
 
-
-    elif 25.0 <= IMC <= 29.9:
+    elif 25.0 <= imc <= 29.9:
         situacao = "perder peso"
 
-
-    elif 30.0 <= IMC <= 34.9:
+    elif 30.0 <= imc <= 34.9:
         situacao = "perder peso"
 
-
-    elif 35.0 <= IMC <= 39.9:
+    elif 35.0 <= imc <= 39.9:
         situacao = "perder peso"
     else:
         situacao = "perder peso"
@@ -84,19 +72,16 @@ def situacao_individuo(IMC):
     return situacao
 
 
-
-IMC = calcular_imc(individuo)
-
-
-classificacao = obter_classificacao(IMC)
+IMC = calcular_imc(pessoa)
 
 
-situacao = situacao_individuo(IMC)
+CLASSIFICACAO = obter_classificacao(IMC)
 
 
-print("Classificacao atual:", classificacao)
+SITUACAO = situacao_individuo(IMC)
 
 
-print("Situacao atual:", situacao)
+print("Classificacao atual:", CLASSIFICACAO)
 
 
+print("Situacao atual:", SITUACAO)
