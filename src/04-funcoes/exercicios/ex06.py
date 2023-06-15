2d45c46b3042d59be9b5f0d13ccb377a506609c9
Ryan Salomão <ryansalomao@gmail.com>
2023-06-15 04:17:02 -0300

    made debugger with breakpoint() & vscode debugger - ex06 Módulo 04

--- src/04-funcoes/exercicios/ex06.py
+++ src/04-funcoes/exercicios/ex06.py
@@ -1,76 +1,117 @@
 """ Exercício 06 """
 
+breakpoint()
+
 
 aquario = {
 
+
     'comprimento': float(input('Digite o comprimento do aquário em (cm): ')),
+
 
     'altura': float(input('Digite a altura do aquário em (cm): ')),
 
+
     'largura': float(input('Digite a largura do aquário em (cm): '))
 
+
 }
+
+
 
 
 termostato = {
 
     'temperatura_ambiente': float(input('Digite a temperatura do ambiente em (graus Cº): ')),
 
+
     'temperatura_desejada': float(input('Digite a temperatura desejada do aquário em (graus Cº): '))
+
 
 }
 
 
-def volume(aquario):
+
+
+def volume_recipiente(recipiente):
+
     """ calcula o volume do aquário """
 
-    comprimento = aquario['comprimento']
 
-    altura = aquario['altura']
+    comprimento = recipiente['comprimento']
 
-    largura = aquario['largura']
+
+    altura = recipiente['altura']
+
+
+    largura = recipiente['largura']
+
 
 
     volume = (comprimento * altura * largura) / 1000
 
 
+
     return volume
 
 
-def potencia_necessaria(volume, termostato):
+
+def potencia_necessaria(volume, temperaturas):
+
+
     """ calcula a potência necessária para o termostato do aquário """
 
-    potencia = volume * 0.05 * (termostato['temperatura_ambiente'] - termostato['temperatura_desejada'])
+
+
+    potencia = volume * 0.05 * (temperaturas['temperatura_ambiente'] - temperaturas['temperatura_desejada'])
 
     return potencia
 
 
+
 def quantidade_filtragem(volume):
+
+
     """ calcula o mínimo de filtragem necessária para o aquário """
+
+
 
     min = volume * 2
 
+
+
     max = volume * 3
 
+
+
     filtragem = f'mínimo de {min} a {max} Litros'
+
+
 
 
     return filtragem 
 
 
 
-VOLUME = volume(aquario)
+
+VOLUME = volume_recipiente(aquario)
+
 
 POTENCIA = potencia_necessaria(VOLUME, termostato)
+
 
 FILTRAGEM = quantidade_filtragem(VOLUME)
 
 
+
 print('Volume do aquário em Litros:', VOLUME)
+
 
 print('Potência do termostato necessaria em Watts:', POTENCIA)
 
+
 print('Filtragem necessaria:', FILTRAGEM)
+
 
 
 
