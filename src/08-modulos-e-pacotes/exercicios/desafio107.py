bc359d387c4fb1835423ff32a2106c4fda019f9f
Ryan Salomão <ryansalomao@gmail.com>
2023-06-21 15:18:01 -0300

    add Desafio 107 - Exercício 01 - Módulo 08

---
+++ src/08-modulos-e-pacotes/exercicios/desafio107.py
@@ -0,0 +1,8 @@
+""" Exercício 01 """
+from ex107 import moeda
+
+p = float(input("Digite o preço: R$ "))
+print(f"A metade de {p} é {moeda.metade(p)}")
+print(f"O drobro de {p} é {moeda.dobro(p)}")
+print(f"Aumentando 10%, temos {moeda.aumentar(p, 10)}")
+print(f"Reduzindo 13%, temos {moeda.diminuir(p, 13)}")
---
+++ src/08-modulos-e-pacotes/exercicios/ex107/moeda/__init__.py
@@ -0,0 +1,16 @@
+""" Funções utilizadas no desafio 107 """
+
+def metade(value):
+    return value / 2
+
+
+def dobro(value):
+    return value * 2
+
+
+def aumentar(value, aumento):
+    return value + ((aumento / 100) * value)
+
+
+def diminuir(value, desconto):
+    return value - ((desconto / 100) * value)
