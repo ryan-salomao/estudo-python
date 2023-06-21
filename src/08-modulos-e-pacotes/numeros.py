5c3e2f6e025656398849a58a2d249329eab4d980
Ryan Salomão <ryansalomao@gmail.com>
2023-06-21 14:57:32 -0300

    add aula Modularização e Pacotes - Módulo 08

---
+++ src/08-modulos-e-pacotes/numeros.py
@@ -0,0 +1,8 @@
+""" Aula - Modularização e Pacotes """
+from uteis import numeros
+
+num = int(input("Digite um valor: "))
+fat = numeros.fatorial(num)
+
+print(f"O fatorial de {num} é {fat}.")
+print(f"O dobro de {num} é {numeros.dobro(num)}.")
